4957793abb9f0aa8efd55d173465a7d19bc8de53
JamieHoSzeYui <henloboii@protonmail.ch>
2020-12-08 09:23:59 +0800

    lancelot : add missing device.mk build fastbootd, inherit gsikeys, define dynamic support.

---
+++ device.mk
@@ -0,0 +1,18 @@
+
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Define Dynamic Partition support
+PRODUCT_TARGET_VNDK_VERSION := 29
+PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_BUILD_PRODUCT_IMAGE := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
